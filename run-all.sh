cc54059996fb3d6bed181ec8228109db53d47f84
UH <554c46@gmail.com>
2022-06-15 22:59:23 +0200

    inference and other settings

--- run-all.sh
+++ run-all.sh
@@ -2,10 +2,15 @@
 
 source .venv/bin/activate
 
+python3 script.py --corr-trgt=0 --corr-regul=0
 python3 script.py --corr-trgt=1 --corr-regul=1
+python3 script.py --corr-trgt=1 --corr-regul=0
 
+python3 script2.py --corr-trgt=0 --corr-regul=0
 python3 script2.py --corr-trgt=1 --corr-regul=1
+python3 script2.py --corr-trgt=1 --corr-regul=0
 
+python3 script3.py --corr-trgt=0 --corr-regul=0
 python3 script3.py --corr-trgt=1 --corr-regul=1
 python3 script3.py --corr-trgt=1 --corr-regul=0
 python3 script3.py --corr-trgt=0 --corr-regul=1
